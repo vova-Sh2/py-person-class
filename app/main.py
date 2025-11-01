37f06b733af8ec900f32a6dc2edb0e123764426f
Vova <vovakucin082@gmail.com>
2025-11-01 15:16:51 +0100

    code write

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    list_person = []
+    for dic in people:
+        person = Person(name=dic["name"], age=dic["age"])
+        list_person.append(person)
+    for index, value in enumerate(people):
+        for i in range(len(people)):
+            if list_person[i].name == value.get("wife"):
+                list_person[index].wife = list_person[i]
+            elif list_person[i].name == value.get("husband") :
+                list_person[index].husband = list_person[i]
+
+    return list_person
